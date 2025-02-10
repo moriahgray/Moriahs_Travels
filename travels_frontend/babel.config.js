bc25049ca93b4247b3bf9dec4275ff45ced9686f
moriahgray <moriah.gray.1@gmail.com>
2025-02-10 14:41:42 -0700

    update some config files

--- travels_frontend/babel.config.js
+++ travels_frontend/babel.config.js
@@ -2,3 +2,17 @@ module.exports = {
     presets: ["babel-preset-expo"],
     plugins: [["module:react-native-dotenv"]],
   };  
+
+  module.exports = {
+    presets: ["module:metro-react-native-babel-preset"],
+    plugins: [
+      "react-native-reanimated/plugin",
+      ["module-resolver", {
+        root: ["./"],
+        alias: {
+          "react-native$": "react-native-web"
+        }
+      }]
+    ]
+  };
+  
---
+++ travels_frontend/metro.config.js
@@ -0,0 +1,9 @@
+const { getDefaultConfig } = require("expo/metro-config");
+
+module.exports = (() => {
+  const config = getDefaultConfig(__dirname);
+
+  config.resolver.assetExts.push("cjs");
+
+  return config;
+})();
