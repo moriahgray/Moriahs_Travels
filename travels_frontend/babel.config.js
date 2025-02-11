210f4f054cfabaf29a37e6f4c43c355c0524d953
moriahgray <moriah.gray.1@gmail.com>
2025-02-11 10:34:02 -0700

    trying to fix my web version

--- travels_frontend/babel.config.js
+++ travels_frontend/babel.config.js
@@ -1,18 +1,21 @@
 module.exports = {
-    presets: ["babel-preset-expo"],
-    plugins: [["module:react-native-dotenv"]],
-  };  
-
-  module.exports = {
-    presets: ["module:metro-react-native-babel-preset"],
-    plugins: [
-      "react-native-reanimated/plugin",
-      ["module-resolver", {
-        root: ["./"],
-        alias: {
-          "react-native$": "react-native-web"
-        }
-      }]
-    ]
-  };
-  
+  presets: [
+    "babel-preset-expo",
+    "module:metro-react-native-babel-preset"
+  ],
+  plugins: [
+    "react-native-reanimated/plugin",
+    ["module:react-native-dotenv", {
+      "moduleName": "@env",
+      "path": ".env",
+      "safe": true,
+      "allowUndefined": false
+    }],
+    ["module-resolver", {
+      root: ["./"],
+      alias: {
+        "react-native$": "react-native-web"
+      }
+    }]
+  ]
+};
