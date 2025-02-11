3b88f810d96b1c1df3c58ba3a16c3669a7aefc6c
moriahgray <moriah.gray.1@gmail.com>
2025-02-11 12:09:53 -0700

    fixing navigation error

--- travels_frontend/src/screens/auth/loginScreen.js
+++ travels_frontend/src/screens/auth/loginScreen.js
@@ -23,10 +23,7 @@ export default function LoginScreen({ navigation, setIsAuthenticated }) {
         // Reset navigation stack to MainMenuScreen
         console.log("Current Navigation State:", navigation.getState());
 
-        navigation.reset({
-          index: 0,
-          routes: [{ name: "MainMenuScreen" }],
-        });
+        navigation.navigate;{"MainMenuScreen"}
 
       } else {
         Alert.alert("Login Error", "Invalid credentials. Please try again.");
