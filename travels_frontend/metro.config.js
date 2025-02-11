2d343f7cdae01d3ae213781d010dc8cd8d7d898c
moriahgray <moriah.gray.1@gmail.com>
2025-02-11 10:48:10 -0700

    cleaning up files

--- travels_frontend/metro.config.js
+++
@@ -1,9 +0,0 @@
-const { getDefaultConfig } = require("expo/metro-config");
-
-module.exports = (() => {
-  const config = getDefaultConfig(__dirname);
-
-  config.resolver.assetExts.push("cjs");
-
-  return config;
-})();
